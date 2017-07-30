0285c2cce4493ec1334135b17cc0a16f32071f2d
FelipePEP <foliveira.webdev@gmail.com>
2017-07-30 10:29:26 -0300

    cors

--- controllers/critical.js
+++ controllers/critical.js
@@ -6,7 +6,7 @@ module.exports = {
     default: async (req, res) => {
         const param = req.query;
         const criticals = await new business(param.type, param.brutality).loadAll();
-        res.send(criticals);
+        res.setHeader('Access-Control-Allow-Origin','*').send(criticals);
         //res.json({ data: await new business(param.type, param.brutality).loadAll() });
     },
     random: async (req, res) => {
