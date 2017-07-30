c276f29eea5c0ba08a2cae2bdfc1d4c7fac2f536
FelipePEP <foliveira.webdev@gmail.com>
2017-07-30 11:00:23 -0300

    cors

--- config/express.js
+++ config/express.js
@@ -10,8 +10,8 @@ module.exports = () => {
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.text());
     app.use(bodyParser.json({ type: 'application/json'}));
-    app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
         next();
